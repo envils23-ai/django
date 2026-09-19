a05b753f8a5fe5abfde29bbc25682c65bd3e0e28
envils <envils23@gmail.com>
2026-09-19 21:54:42 +0300

    initial commit

--- pycharm/settings.py
+++ pycharm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7g4i+#!7pndu)i#pi%y&we87=^$_g!xs92td!lb=(@e&z!rbav
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
